7e524085c7880c850641bc7b29a53acaa95a4817
mimenza <mimenzae@gmail.com>
2023-12-14 16:50:02 +0100

    checker and push_swap linked

--- checker/incs/checker.h
+++ checker/incs/checker.h
@@ -6,7 +6,7 @@
 /*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 12:33:00 by emimenza          #+#    #+#             */
-/*   Updated: 2023/12/14 12:45:39 by emimenza         ###   ########.fr       */
+/*   Updated: 2023/12/14 16:37:16 by emimenza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include "../../push_swap/incs/push_swap.h"
 # include "../libs/get_next_line/get_next_line.h"
+# include "../../push_swap/libs/ft_printf/ft_printf.h"
+# include "../../push_swap/libs/Libft/libft.h"
+
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wunused-function"
+# include "../../push_swap/incs/push_swap.h"
+# pragma GCC diagnostic pop
 
 #endif
